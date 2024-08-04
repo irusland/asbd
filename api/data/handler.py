084748314e595a7f362e7d412328bbe5968c47d6
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2024-08-04 23:17:16 +0300

    add api tests

--- api/data/handler.py
+++ api/data/handler.py
@@ -11,7 +11,11 @@ class DataHandler:
         self._data_clients = data_clients
 
     async def read_data(self) -> list[Data]:
-        data_parts = await gather(
-            *[data_client.get_data() for data_client in self._data_clients]
+        coroutines = [data_client.get_data() for data_client in self._data_clients]
+
+        data_parts_results = await gather(*coroutines, return_exceptions=True)
+        data = chain.from_iterable(
+            data_result for data_result in data_parts_results if
+            not isinstance(data_result, BaseException)
         )
-        return sorted(chain(*data_parts), key=operator.attrgetter("id"))
+        return sorted(data, key=operator.attrgetter("id"))
--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -7,10 +7,18 @@ from api.data.clients.memory import MemoryDataClient
 from api.deps import DepsContainer
 from api.server import Server
 
+
 @pytest.fixture()
-def override_memory_data_client(deps_container: DepsContainer) -> None:
+def memory_data_client(mocker) -> MemoryDataClient:
+    client = MemoryDataClient()
+    client.get_data = mocker.AsyncMock(wraps=client.get_data)
+    return client
+
+
+@pytest.fixture()
+def override_memory_data_client(deps_container: DepsContainer, memory_data_client: MemoryDataClient) -> None:
     deps_container.registrations[IDataClient].clear()
-    deps_container.register(IDataClient, MemoryDataClient, scope=Scope.singleton)
+    deps_container.register(IDataClient, instance=memory_data_client, scope=Scope.singleton)
 
 
 @pytest.fixture()
@@ -23,8 +31,32 @@ def client(server: Server) -> TestClient:
     return TestClient(server)
 
 
+def async_raise_func(exception: Exception):
+    async def _async_raise_func():
+        raise exception
+    return _async_raise_func
+
+
 class TestServer:
     def test_gets_data(self, client: TestClient):
         response = client.get("/data")
+        assert response.status_code == 200
+        assert response.json() == []
+
+    @pytest.mark.asyncio
+    async def test_ignores_data_when_error(self, client: TestClient, memory_data_client: MemoryDataClient):
+        memory_data_client.get_data = async_raise_func(Exception('Error'))
+
+        response = client.get("/data")
+
+        assert response.status_code == 200
+        assert response.json() == []
+
+    @pytest.mark.asyncio
+    async def test_ignores_data_when_timeout_error(self, client: TestClient, memory_data_client: MemoryDataClient):
+        memory_data_client.get_data = async_raise_func(TimeoutError())
+
+        response = client.get("/data")
+
         assert response.status_code == 200
         assert response.json() == []
